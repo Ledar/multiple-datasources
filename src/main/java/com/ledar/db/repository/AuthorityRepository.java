a93dd7e23eda97602f05cd632a2f734fce0675d3
LiChunhao <rememberyou72@gmail.com>
2022-03-07 11:12:28 +0800

    Initial version of db generated by generator-jhipster@7.7.0

---
+++ src/main/java/com/ledar/db/repository/AuthorityRepository.java
@@ -0,0 +1,9 @@
+package com.ledar.db.repository;
+
+import com.ledar.db.domain.Authority;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {}
---
+++ src/main/java/com/ledar/db/web/rest/vm/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * View Models used by Spring MVC REST controllers.
+ */
+package com.ledar.db.web.rest.vm;
